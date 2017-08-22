42403257dea93250908bf71ee297b286f7a4055d
Patrick Holthaus <patrick.holthaus@uni-bielefeld.de>
2017-08-22 11:41:45 +0200

    uncert

--- position.py
+++ position.py
@@ -59,11 +59,12 @@ def main(session):
             if i == len(hist) - 1:
                 col = (0,255,0)
                 mid = (0, 0, 0)
-                buf.putpixel((r[0], math.max(0, math.min(map_height, r[1] - unc_y)), col)
+
+                buf.putpixel((r[0], math.max(0, math.min(map_height, r[1] - unc_y))), col)
                 buf.putpixel((math.max(0, math.min(map_width, r[0] - unc_x)), r[1]), col)
                 buf.putpixel((r[0], r[1]), mid)
+                buf.putpixel((r[0], math.max(0, math.min(map_height, r[1] + unc_y))), col)
                 buf.putpixel(((math.max(0, math.min(map_width, r[0] + unc_x)), r[1]), col)
-                buf.putpixel((r[0], math.max(0, math.min(map_height, r[1] + unc_y)), col)
             else:
                 buf.putpixel((r[0], r[1]), (20, 100, 20))
 
