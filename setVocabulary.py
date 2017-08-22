9424f8a7537597d145e26ed1e4a23e2e661c6909
Patrick Holthaus <patrick.holthaus@uni-bielefeld.de>
2017-08-22 10:02:27 +0200

    typos, default robbie

--- setVocabulary.py
+++ setVocabulary.py
@@ -9,8 +9,8 @@ def main(session):
     asr_service = session.service("ALSpeechRecognition")
     asr_service.setVocabulary({"Ja","Nein","Schwert","Schild"])
 
-if __name__ == "__main__"
-    parser = argpase.ArgumentParser()
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
     parser.add_argument("--ip", type=str, default="pepper.local",
                         help="Robot IP Address")
     parser.add_argument("--port", type=str, default=9559,
--- stream.sh
+++ stream.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-gst-launch-0.10 multifilesrc caps=image/jpeg,framerate=20/1 location=/tmp/current.jpg loop=true ! decodebin ! jpegenc ! rtpjpegpay ! udpsink host=pepper port=3000
+#gst-launch-0.10 multifilesrc caps=image/jpeg,framerate=20/1 location=/tmp/current.jpg loop=true ! decodebin ! jpegenc ! rtpjpegpay ! udpsink host=pepper port=3000
+gst-launch-0.10 multifilesrc location=/tmp/current.jpg loop=true ! decodebin ! jpegenc ! rtpjpegpay ! udpsink host=pepper port=3000
